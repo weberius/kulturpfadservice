4a8e8210f41729f73a308f2aab26ca1694f41032
Wolfram Eberius <eberius@gmail.com>
2023-04-19 22:20:49 +0200

    #8 add RouteServiceForGPX

--- src/main/java/de/illilli/kulturpfade/controller/KulturpfadeController.java
+++ src/main/java/de/illilli/kulturpfade/controller/KulturpfadeController.java
@@ -4,11 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.illilli.kulturpfade.services.PoiService;
 import de.illilli.kulturpfade.services.RouteServiceForDataTable;
 import de.illilli.kulturpfade.services.RouteServiceForFeatureCollection;
+import de.illilli.kulturpfade.services.RouteServiceForGPX;
+import io.jenetics.jpx.GPX;
 import org.geojson.FeatureCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/service")
@@ -49,6 +61,27 @@ public class KulturpfadeController {
 
         RouteServiceForDataTable service = new RouteServiceForDataTable(id);
         return new ObjectMapper().writeValueAsString(service.getData());
+
+    }
+
+    @GetMapping("/gpx/{id}")
+    public ResponseEntity getGpx(@PathVariable String id) throws Exception {
+
+        // getting '<id>.json'; need '<id>'
+        id = id.substring(0, id.indexOf('.'));
+
+        // save gpx-file to filesystem
+        new RouteServiceForGPX(id);
+
+        // read from filesystem
+        String filePath = "./" + id + ".gpx";
+        InputStream inputStream = new FileInputStream(new File(filePath));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentLength(Files.size(Paths.get(filePath)));
+
+        // return gpx file
+        return new ResponseEntity(inputStreamResource, headers, HttpStatus.OK);
 
     }
 
