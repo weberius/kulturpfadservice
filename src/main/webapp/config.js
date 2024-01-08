81e06b0b09f5d1cd79906ec0db150c4bb3f52155
Wolfram Eberius <wolfram.eberius@rewe-group.com>
2024-01-08 21:35:04 +0100

    kulturpfade data

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -2,17 +2,17 @@ var config = {
 	"requireArcGISLogin": false, // Does the user need to log in to ArcGIS Online or ArcGIS Server?
 	"tokenUrl": 'https://www.arcgis.com/sharing/generateToken', // ArcGIS token generation URL
 
-	"title": "Fotopfad Fritzlar",
+	"title": "Kulturpfade Köln",
 	"start": {
 		// "maxZoom": 16,
-		"center": [51.1318,9.27438],
-		"zoom": 17,
+		"center": [50.941,6.958],
+		"zoom": 13,
 		"attributionControl": true,
 		"zoomControl": false,
 		"data": "data"
 	},
 	"about": {
-		"title": "Fotopfad Fritzlar",
+		"title": "Kulturpfade",
 		"contents": "<p>This is an open-source version of the excellent <a href='https://github.com/bmcbride/bootleaf'>Bootleaf map </a>started by Bryan McBride.</p><p>It's designed for rapid web map development. See <a href='https://github.com/iag-geo/bootleaf'>https://github.com/iag-geo/bootleaf</a> for more information.</p><p>Chage this message in the config file</p>"
 	},
 	"controls": {
